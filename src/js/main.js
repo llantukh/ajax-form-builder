58e917da22943d979eb551a1427c1411b7caa9a8
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-26 19:10:04 -0400

    changed inputs to match types

--- src/js/main.js
+++ src/js/main.js
@@ -1,119 +1,42 @@
-//firstname <i class="fa fa-user" aria-hidden="true"></i>
-//lastname <i class="fa fa-user" aria-hidden="true"></i>
-// email <i class="fa fa-envelope" aria-hidden="true"></i>
-// website <i class="fa fa-globe" aria-hidden="true"></i>
-// comments <i class="fa fa-comments" aria-hidden="true"></i>
-// mobile number <i class="fa fa-comments" aria-hidden="true"></i>
-// home number <i class="fa fa-phone" aria-hidden="true"></i>
-
-
-//
-
 import $ from 'jquery';
 
 
 var data = $.ajax({
   url: "http://json-data.herokuapp.com/forms",
 });
-console.log(data);
-
-
+// console.log(data);
 
 function makeForm (item) {
-for (var i = 0; i < item.length; i++){
-  // if (item[i].label === "Last Name"){
-    var resultsHTML = `
-      <div class="oneLiners">
-      <input type="text" placeholder=" ${item[i].label}">
-
+  for (var i = 0; i < item.length; i++){
+    if (item[i].label === "Select Language") {
+      var resultsHTML = `
+        <div class = "dropDown">
+          <select class="Select Language" name="select">
+            <option value="value1">${item[i].options[0].label}</option>
+            <option value="value1">${item[i].options[1].label}</option>
+            <option value="value1">${item[i].options[2].label}</option>
+            <option value="value1">${item[i].options[3].label}</option>
+            <option value="value1">${item[i].options[4].label}</option>
+          </select>
+        </div>
+      `;
+    } else if (item[i].label === "Your Comment") {
+      var resultsHTML = `
+      <div class="commentBox">
+      <i class="fa ${item[i].icon}"></i>
+      <textarea class="form" id="${item[i].id}" placeholder="${item[i].label}"></textarea>
       </div>
-     `
-   $(".container").append(resultsHTML);
-// }
- }
+      `;
+    } else {
+    var resultsHTML = `
+    <div class="other">
+    <i class="fa ${item[i].icon}"></i>
+    <input class="otherInfo" id="${item[i].id}" type="text" placeholder="${item[i].label}">
+    </div>
+    `;
+    };
+     $(".container").append(resultsHTML);
+   }
 };
-
+// data.then(console.log);
 data.then(makeForm);
-
-
-
-
-  //   console.log();
-  // }
-  // } else {
-  // if (data[i].type === "email") {
-  //   return ``
-  // } else {
-  // if (data[i].type === "select") {
-  //   return ``
-  // } else {
-  // if (data[i].type === "textarea") {
-  //   return ``
-  // } else {
-  //   if (data[i].type === "tel") {
-  //   return ``
-  //   }
-  // }
-  // }
-  // }
-  // }
-//
-//   $(".container").append(makeForm);
-// };
-//
-
-
-
-// {
-//   var HTML = `<input type="text" placeholder="poo">`;
-//   $(".container").html(HTML);
-//   console.log(data);
-// };
-
-//
-
-
-
-
-
-// // pull the data we need:
-//
-// function pullData (data){
-//   var dataArray = [];
-//   for (var i= 0; i< data.results.length; i++){
-//
-//   var itemName = data.results[i].title;
-//   var itemURL = data.results[i].url;
-//   var price = data.results[i].price;
-//   var image = data.results[i].Images[0].url_fullxfull;
-//   var storeName = data.results[i].Shop.shop_name;
-//   var storeURL = data.results[i].Shop.url;
-//
-//
-// // get the data structured how you want it on the page:
-//
-//   var resultHTML = `
-//     <div class = "singleItem">
-//       <img src = ${image}>
-//       <div>
-//         <a href = ${itemURL}>
-//         ${itemName}
-//         </a>
-//          <div>
-//           <span class = "store"> <a href = ${storeURL}>${storeName} </a></span>
-//           <span class = "price"> $${price} <span>
-//         </div>
-//       </div>
-//     </div>`;
-//
-// // get the data onto the page:
-//
-//     $(".searchResults").append(resultHTML);
-//
-// }
-// };
-//
-//
-// // run the function we just wrote.
-//
-// pullData(allData);
