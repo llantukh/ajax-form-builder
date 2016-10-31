b3781eef7a9e4ec02343e6e27b8cbd0eddd54671
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-10-30 21:16:43 -0400

    got everything where it should be except for the icons

--- src/js/main.js
+++ src/js/main.js
@@ -11,27 +11,28 @@ function makeForm (item) {
     if (item[i].label === "Select Language") {
       var resultsHTML = `
         <div class = "dropDown">
-          <select class="Select Language" name="select">
-            <option value="value1">${item[i].options[0].label}</option>
-            <option value="value1">${item[i].options[1].label}</option>
-            <option value="value1">${item[i].options[2].label}</option>
-            <option value="value1">${item[i].options[3].label}</option>
-            <option value="value1">${item[i].options[4].label}</option>
+          <select class="language">
+            <option> Select Language</option>
+            <option> ${item[i].options[0].label}</option>
+            <option> ${item[i].options[1].label}</option>
+            <option> ${item[i].options[2].label}</option>
+            <option> ${item[i].options[3].label}</option>
+            <option> ${item[i].options[4].label}</option>
           </select>
         </div>
       `;
     } else if (item[i].label === "Your Comment") {
       var resultsHTML = `
       <div class="commentBox">
-      <i class="fa ${item[i].icon}"></i>
-      <textarea class="form" id="${item[i].id}" placeholder="${item[i].label}"></textarea>
+      <i class="smallIcon fa ${item[i].icon}"></i>
+      <textarea class="form" placeholder="${item[i].label}"></textarea>
       </div>
       `;
     } else {
     var resultsHTML = `
     <div class="other">
-    <i class="fa ${item[i].icon}"></i>
-    <input class="otherInfo" id="${item[i].id}" type="text" placeholder="${item[i].label}">
+    <i class="smallIcon fa ${item[i].icon}"></i>
+    <input class="otherInfo" placeholder="${item[i].label}">
     </div>
     `;
     };
